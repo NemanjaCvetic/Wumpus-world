9da20913517870403ce0e88ac734316f841c064a
NemanjaCvetic <cveticnemanja26@gmail.com>
2024-09-07 20:15:07 +0200

    deleted unnecesary test worlds

--- Wumpusworld/obj/Debug/net8.0/Wumpusworld.AssemblyInfo.cs
+++ Wumpusworld/obj/Debug/net8.0/Wumpusworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wumpusworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d1d5a03125ac2d38a2b6215ea4be9eb8dde365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a7dac12011633bb56d8f639a89d8f2ec68bab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wumpusworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wumpusworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
